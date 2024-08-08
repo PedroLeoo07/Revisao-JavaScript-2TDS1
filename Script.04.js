87f9b27e9a063647cf98f5a1d736ac0cab78c0f1
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-08-08 09:40:23 -0300

    Arquivo novo

---
+++ Script.04.js
@@ -0,0 +1,14 @@
+let armas = ["AK-47", "MP5", "Calça Angelical azul", "12 nova"];
+
+
+let ataques = [];
+
+armas.push(armas[0]);
+armas.push(armas[1]);
+armas.push(armas[2]);
+armas.push(armas[3]);
+
+for (let i = 0; i < armas.length; i+=1){
+    if (ataques[0] == ataques[1])
+    console.log("Top dmaizi")
+}
