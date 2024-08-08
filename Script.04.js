c32def467a1c0f362e6fb2d1c273cc688d653b12
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-08-08 11:06:11 -0300

    Arquivo completo

--- Script.04.js
+++ Script.04.js
@@ -1,14 +1,19 @@
-let armas = ["AK-47", "MP5", "Calça Angelical azul", "12 nova"];
-
-
+let armas = ["AK-47", "MP5", "Gelo Invertido", "G18", "Granada"];
 let ataques = [];
+let NomeRobo = "Fúria da Noite"
 
+armas.push("Fúria da Noite");
 armas.push(armas[0]);
 armas.push(armas[1]);
 armas.push(armas[2]);
 armas.push(armas[3]);
+armas.push(armas[4]);
 
-for (let i = 0; i < armas.length; i+=1){
-    if (ataques[0] == ataques[1])
-    console.log("Top dmaizi")
-}
+    for (let i = 0; i < ataques.length; i++) {
+        if (i == o) {
+            console.log(`Nome do Robô: ${ataques[i]}`);
+        } else{
+            console.log(`Ataques ${i}: ${ataques[i]}`);
+        }
+    }
+   
